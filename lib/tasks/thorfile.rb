b3754bdfaea4bc6e88625a7e3e66370443502b0a
Daniel Martin <etd-github@nomejortu.com>
2016-11-02 22:18:32 +0100

    Update Thor task

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,26 +1,39 @@
-class DradisTasks < Thor
-  class Upload < Thor
-    namespace "dradis:upload"
+class ZapTasks < Thor
+  include Core::Pro::ProjectScopedTask if defined?(::Core::Pro)
 
-   desc  "zap FILE", "upload ZAP results"
-   long_desc "This will appear if the user runs 'thor help dradis:upload:zap'"
-   def zap(file_path)
-     require 'config/environment'
+  namespace "dradis:plugins:zap"
 
-     logger = Logger.new(STDOUT)
-     logger.level = Logger::DEBUG
+  desc  "upload FILE", "upload ZAP XML results"
+  def upload(file_path)
+    require 'config/environment'
 
-     unless File.exists?(file_path)
-       $stderr.puts "** the file [#{file_path}] does not exist"
-       exit -1
-     end
+    logger = Logger.new(STDOUT)
+    logger.level = Logger::DEBUG
 
-     ZapUpload.import(
-       :file => file_path,
-       :logger => logger)
+    unless File.exists?(file_path)
+      $stderr.puts "** the file [#{file_path}] does not exist"
+      exit -1
+    end
 
-     logger.close
-   end
+    content_service = nil
+    template_service = nil
 
+    template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Zap)
+    if defined?(Dradis::Pro)
+      detect_and_set_project_scope
+      content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Zap)
+    else
+      content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Zap)
+    end
+
+    importer = Dradis::Plugins::Zap::Importer.new(
+                logger: logger,
+       content_service: content_service,
+      template_service: template_service
+    )
+
+    importer.import(file: file_path)
+
+    logger.close
   end
 end
