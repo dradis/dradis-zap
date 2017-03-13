5a12ececba51170c7210203e66d324aee8b36eab
Daniel Martin <etd-github@nomejortu.com>
2017-03-13 11:40:59 +0100

    Prepare fro dradis-plugins 3.6

--- lib/tasks/thorfile.rb
+++ lib/tasks/thorfile.rb
@@ -1,5 +1,5 @@
 class ZapTasks < Thor
-  include Core::Pro::ProjectScopedTask if defined?(::Core::Pro)
+  include Rails.application.config.dradis.thor_helper_module
 
   namespace "dradis:plugins:zap"
 
@@ -15,23 +15,9 @@ class ZapTasks < Thor
       exit -1
     end
 
-    content_service = nil
-    template_service = nil
+    detect_and_set_project_scope
 
-    template_service = Dradis::Plugins::TemplateService.new(plugin: Dradis::Plugins::Zap)
-    if defined?(Dradis::Pro)
-      detect_and_set_project_scope
-      content_service = Dradis::Pro::Plugins::ContentService.new(plugin: Dradis::Plugins::Zap)
-    else
-      content_service = Dradis::Plugins::ContentService.new(plugin: Dradis::Plugins::Zap)
-    end
-
-    importer = Dradis::Plugins::Zap::Importer.new(
-                logger: logger,
-       content_service: content_service,
-      template_service: template_service
-    )
-
+    importer = Dradis::Plugins::Zap::Importer.new(logger: logger)
     importer.import(file: file_path)
 
     logger.close
