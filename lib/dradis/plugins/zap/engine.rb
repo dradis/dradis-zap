ba81c98c9fa6e000cdd05fccf1d6d0adcd8e94fd
Brian Pearce <brian.o.pearce@gmail.com>
2022-03-17 15:24:29 +0100

    Register template names locally

--- lib/dradis/plugins/zap/engine.rb
+++ lib/dradis/plugins/zap/engine.rb
@@ -5,5 +5,9 @@ module Dradis::Plugins::Zap
     include ::Dradis::Plugins::Base
     description 'Processes ZAP XML format'
     provides :upload
+
+    def self.template_names
+      { module_parent => { evidence: 'evidence', issue: 'issue' } }
+    end
   end
 end
