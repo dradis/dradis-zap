7f277f7a33a76759340bebabd3a879a4a43e7ea2
Matt Budz <matt.budz@protonmail.com>
2024-03-08 12:03:28 +0100

    add default mappings

--- lib/dradis/plugins/zap/mapping.rb
+++ lib/dradis/plugins/zap/mapping.rb
@@ -1,6 +1,25 @@
 module Dradis::Plugins::Zap
   module Mapping
     def self.default_mapping
+      {
+        'evidence' => {
+          'Description' => "URI: {{ zap[evidence.uri] }}\n
+                            Param: {{ zap[evidence.param] }}\n
+                            Attack:\n
+                            bc.. {{ zap[evidence.attack] }}"
+        },
+        'issue' => {
+          'Title' => '{{ zap[issue.alert] }}',
+          'Risk' => '{{ zap[issue.riskdesc] }}',
+          'Confidence' => '{{ zap[issue.confidence] }}',
+          'Description' => '{{ zap[issue.desc] }}',
+          'Solution' => '{{ zap[issue.solution] }}',
+          'OtherInfo' => '{{ zap[issue.otherinfo] }}',
+          'References' => "{{ zap[issue.reference] }}\n
+                          CWE: {{ zap[issue.cweid] }}\n
+                          WASC: {{ zap[issue.wascid] }}"
+        }
+      }
     end
   end
 end
